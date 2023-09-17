ded01d380babb6e6235d99b3c90e96b4a813ee85
lewkevin6 <112120323+lewkevin6@users.noreply.github.com>
2023-09-17 22:31:08 +0000

    Modified app to include loading functions

--- app.py
+++ app.py
@@ -1,5 +1,31 @@
-#Control shift / for aplite
 import streamlit as st
+import joblib
+import pandas as pd
 
-st.write("Hello World")
+
+#Loads the desired model in for the app to use
+def load_model():
+    return joblib.load('model_for_app.pkl')
+
+#Loads in the cleaned data
+def load_data():
+    return pd.read_csv('cleaned_data.csv')
+
+
+data = load_data()
+
+
+
+#Editing the side bar
+with st.sidebar:
+    st.write("Welcome to the stroke prediction app")
+    st.radio('Menu:', ["Predict", "Data used"])
+
+
+
+st.radio('Gender', ['Male', 'Female'])
+
+
+
+st.write(data)
 
