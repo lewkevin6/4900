50358c9d28be71d766a0739e0c8182f4fd43bd68
lewkevin6 <112120323+lewkevin6@users.noreply.github.com>
2023-10-12 21:47:56 +0000

    Implementing new pipeline into app

--- app.py
+++ app.py
@@ -6,8 +6,7 @@ st.title("Stroke Predictor")
 
 #Loads the desired model in for the app to use
 def load_model():
-    return joblib.load('/workspaces/4900/data_cleaning_1/model_for_app_1.pkl')
-
+    return joblib.load('/workspaces/4900/pipeline_for_app_3.pkl')
 
 gender = st.radio('Gender', ['Male', 'Female'])
 age = st.number_input("Enter your age:")
@@ -20,11 +19,11 @@ avg_glucose_level = st.number_input("Enter your average glucose level:")
 bmi = st.number_input("Enter your BMI: ")
 smoking_status = st.radio('Smoking status', ['Never smoked', 'Unknown', 'Formerly smoked', 'Smokes'])
 
-
 #features = ['gender', 'age', 'hypertension', 'heart_disease', 'ever_married', 'work_type', 'Residence_type', 'avg_glucose_level', "bmi", "smoking_status"]
 def predict(gender, age, hypertension, heart_disease, married, work_type, residence_type, avg_glucose_level, bmi, smoking_status):
     
-    model = load_model()
+    pipeline = load_model()
+
     flag = True
 
     if(age <= 0):
@@ -38,55 +37,20 @@ def predict(gender, age, hypertension, heart_disease, married, work_type, reside
     if(bmi <= 0):
         st.error("Your bmi is below 0, please enter a valid bmi")
         flag = False
-
-    if(gender == "Male"):
-        converted_gender = 1
-    elif(gender == "Female"):
-        converted_gender = 0
-
-    if(hypertension == "Yes"):
-        converted_hypertension = 1
-    elif(hypertension == "No"):
-        converted_hypertension = 0
-
-    if(heart_disease == "Yes"):
-        converted_heart_disease = 1
-    elif(heart_disease == "No"):
-        converted_heart_disease = 0
-
-    if(married == "Yes"):
-        converted_married = 1
-    elif(married == "No"):
-        converted_married = 0
-
-    if(work_type == "Private"):
-        converted_work_type = 0
-    elif(work_type == "Self-Employed"):
-        converted_work_type = 1
-    elif(work_type == "Children"):
-        converted_work_type = 2
-    elif(work_type == "Government Job"):
-        converted_work_type = 3
-    elif(work_type == "Never worked"):
-        converted_work_type = 4
-
-    if(residence_type == "Rural"):
-        converted_residence_type = 0
-    elif(residence_type == "Urban"):
-         converted_residence_type = 1
-
-    if(smoking_status == "Never smoked"):
-        converted_smoking_status = 0
-    elif(smoking_status == "Unknown"):
-         converted_smoking_status = 1
-    elif(smoking_status == "Formerly smoked"):
-         converted_smoking_status = 2
-    elif(smoking_status == "Smokes"):
-         converted_smoking_status = 3
-
-    prediction = model.predict([[converted_gender,age,converted_hypertension,converted_heart_disease,converted_married,converted_work_type,converted_residence_type,avg_glucose_level, bmi, converted_smoking_status]])
     
-    if(flag == True):
+    if (hypertension == 'No'):
+        hypertension = 0
+    elif(hypertension == 'Yes'):
+        hypertension = 1
+
+    if (heart_disease == 'No'):
+        heart_disease = 0
+    elif(heart_disease == 'Yes'):
+        heart_disease = 1
+
+    prediction = pipeline.predict([[gender, age, hypertension, heart_disease, married, work_type, residence_type, avg_glucose_level, bmi, smoking_status]])
+    
+    if(flag is True):
         if(prediction == 0):
             st.success("No Stroke")
         elif(prediction == 1):
